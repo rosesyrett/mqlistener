cdb0e35216aee76f2a4285361f165eef486ec875
Rose Yemelyanova <rose.yemelyanova@diamond.ac.uk>
2023-10-30 16:53:37 +0000

    Remove CI job

--- src/mqlistener/__main__.py
+++ src/mqlistener/__main__.py
@@ -6,12 +6,14 @@ import stomp
 
 LOGGER = logging.getLogger("mqlistener")
 
+
 class Listener(stomp.ConnectionListener):
     def on_error(self, frame):
-        LOGGER.info(f'ERROR: {frame}')
+        LOGGER.info(f"ERROR: {frame}")
 
     def on_message(self, frame):
-        LOGGER.info(f'{frame.body}')
+        LOGGER.info(f"{frame.body}")
+
 
 def run(host, port, topic, username, password):
     if (not username) or (not password) or (not topic):
@@ -22,9 +24,9 @@ def run(host, port, topic, username, password):
         LOGGER.info(f"LISTENER: CONNECTING: host {host} port {port}")
 
         listen = Listener()
-        conn.set_listener('printing', listen)
+        conn.set_listener("printing", listen)
         conn.connect(username=username, passcode=password)
-        conn.subscribe(destination=topic, id=1, ack='auto')
+        conn.subscribe(destination=topic, id=1, ack="auto")
 
         LOGGER.info(f"LISTENER: DESTINATION: set to {topic}")
 
@@ -34,6 +36,7 @@ def run(host, port, topic, username, password):
         except KeyboardInterrupt:
             print("Stopped with keyboard")
 
+
 def main():
     host = os.getenv("HOST", "localhost")
     port = os.environ.get("PORT", "61613")
@@ -42,7 +45,7 @@ def main():
     topic = os.environ.get("TOPIC")
     run(host, port, topic, username, password)
 
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     main()
-
